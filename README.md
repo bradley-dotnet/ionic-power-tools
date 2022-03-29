4b12983cbd7f04db45576ad27a8350c95ac3d53e
Bradley Carestia <bradley.dotnet@gmail.com>
2022-03-28 19:37:13 -0700

    Initial files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ionic-power-tools
 Supercharging Ionic Development
+
+## Licensing
+This project is licensed under GPL v3. Alternative licensing can be obtained by contacting the author.
---
+++ projects/ionic-power-tools/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of ionic-power-tools
+ */
+
+export * from './lib/dynamic-modal/dynamic-modal.module';
+export * from './lib/dynamic-router/dynamic-router.module';
+export * from './lib/forms/forms.module';
+export * from './lib/loading-spinner/loading-spinner.module'
